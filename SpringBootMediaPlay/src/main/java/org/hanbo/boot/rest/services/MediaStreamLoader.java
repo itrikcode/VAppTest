20fa7ad7201a3c3473967b6907d8257de8743374
vivek kumar <kumarviveklohaman@gmail.com>
2023-03-18 09:51:21 +0530

    first commit

---
+++ SpringBootMediaPlay/src/main/java/org/hanbo/boot/rest/services/MediaStreamLoader.java
@@ -0,0 +1,15 @@
+package org.hanbo.boot.rest.services;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+public interface MediaStreamLoader
+{
+   ResponseEntity<StreamingResponseBody> loadEntireMediaFile(String localMediaFilePath) throws IOException;
+
+   ResponseEntity<StreamingResponseBody> loadPartialMediaFile(String localMediaFilePath, String rangeValues) throws IOException;
+
+   ResponseEntity<StreamingResponseBody> loadPartialMediaFile(String localMediaFilePath, long fileStartPos, long fileEndPos) throws IOException;
+}
